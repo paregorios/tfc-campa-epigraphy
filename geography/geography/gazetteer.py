9e4b4c9ef45419dcbeaf0cf5c98ac32a75b24dbb
Tom Elliott <tom.elliott@nyu.edu>
2021-03-17 13:43:56 -0500

    transfer gazetteer class

--- geography/geography/gazetteer.py
+++ geography/geography/gazetteer.py
@@ -1,10 +1,49 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Python 3 script template (changeme)
+Gazetteer class for Campā parser
 """
 
-import better_exceptions
+from geography.catalog 
+from geography.logger import SelfLogger
 import logging
 
-logger = logging.getLogger(__name__)
+
+class Gazetteer(SelfLogger):
+
+    def __init__(self):
+        self.places = {}
+        self.catalog = {
+            'names2pids': PlaceIndexByName()
+        }
+
+    def set_place(self, place, overwrite=False):
+        try:
+            self.places[place.pid]
+        except KeyError:
+            self.places[place.pid] = place
+        else:
+            if overwrite:
+                logger.warning(
+                    'Overwriting {}'.format(place.pid)
+                )
+                self.places[place.pid] = place
+            else:
+                raise NotImplementedError('place collision')
+            return
+        self.catalog['names2pids'].add(place)
+
+    def lookup(self, term):
+        try:
+            hit = self.places[term]
+        except KeyError:
+            try:
+                hit = self.places[self.catalog['names2pids'].lookup(term)]
+            except KeyError:
+                hit = None
+        if hit is None:
+            raise LookupError('Could not find {}'.format(term))
+        else:
+            return hit
+
+
